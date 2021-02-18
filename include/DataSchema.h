9d67aafcc28f5b67759dc1ef2feaf7e867f639b7
Ole Petersen <peteole2707@gmail.com>
2021-02-18 16:43:11 +0100

    Added message sending

--- include/DataSchema.h
+++ include/DataSchema.h
@@ -7,20 +7,35 @@ struct SensorValue
     uint16_t size;
 
 public:
-    virtual String toJSON()=0;
+    virtual String toJSON() = 0;
+    virtual char *getMessage() = 0;
 };
 struct BasicSensorValue : public SensorValue
 {
     String type;
     String toJSON() override;
 };
+
 struct SensorValueList : public SensorValue
 {
+private:
+    char *messageBuffer;
+
 public:
-    SensorValue *Sensorvalues;
+    SensorValue *sensorvalues;
     uint16_t length;
     String toJSON() override;
+    char *getMessage() override;
+    SensorValueList(SensorValue *sensorvalues, uint16_t length);
 };
 
-
+// A message type which allows setting sensor values and converting them to a sendable format
+class MessageTemplate
+{
+    // uinique ID for this kind of message. Is sent at the beginning of each message.
+    uint8_t id;
+    SensorValue *value;
+    MessageTemplate(SensorValue *value, uint8_t id);
+    void send(Stream *medium);
+};
 #endif
--- src/DataSchema.cpp
+++ src/DataSchema.cpp
@@ -1,12 +1,30 @@
 #include "DataSchema.h"
-String BasicSensorValue::toJSON(){
-    return "{name:"+this->name+", size: "+this->size+", type: "+this->type;
+String BasicSensorValue::toJSON()
+{
+    return "{name:" + this->name + ", size: " + this->size + ", type: " + this->type;
 }
-String SensorValueList::toJSON(){
-    String JSON= "{name:"+this->name+", size: "+this->size+", values:[\n";
-    for(int i=0;i<this->length;i++){
-        JSON+=this->Sensorvalues[i].toJSON()+",\n";
+char *SensorValueList::getMessage()
+{
+}
+String SensorValueList::toJSON()
+{
+    String JSON = "{name:" + this->name + ", size: " + this->size + ", values:[\n";
+    for (int i = 0; i < this->length; i++)
+    {
+        JSON += this->sensorvalues[i].toJSON() + ",\n";
     }
-    JSON+="]\n}";
+    JSON += "]\n}";
     return JSON;
+}
+
+SensorValueList::SensorValueList(SensorValue *sensorvalues, uint16_t length)
+{
+    this->sensorvalues = sensorvalues;
+    this->length = length;
+    this->size = 0;
+    for (int i = 0; i < this->length; i++)
+    {
+        this->size += this->sensorvalues[i].size;
+    }
+    this->messageBuffer = new char[this->size];
 }
